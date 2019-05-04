80cc5321c84175a053fa9d6c08b40109b2d97ca9
Swathi Prasad <swathi.prasad88@gmail.com>
2019-05-04 22:10:47 +0200

    Add ag-grid component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-codelyzer';
+  title = 'Angular ag-grid';
+
+  columnDefs = [
+        { headerName: 'Employee Name', field: 'name', sortable:true, filter:true, checkboxSelection: true },
+        { headerName: 'Title', field: 'title', sortable:true, filter:true },
+        { headerName: 'Employee Number', field: 'number', sortable:true, filter:true },
+        { headerName: 'Date of Joining', field: 'date', sortable:true, filter:true }
+    ];
+
+    rowData = [
+        { name: 'John', title: 'Software Engineer', number: 123456, date: 'March 02, 2016' },
+        { name: 'Jane', title: 'Senior Software Engineer', number: 123451, date: 'April 01, 2014' },
+        { name: 'Richard', title: 'Software Engineer', number: 123452, date: 'January 02, 2015' },
+        { name: 'Janie', title: 'Software Engineer', number: 123453, date: 'March 23, 2016' },
+        { name: 'Johnny', title: 'Senior Software Engineer', number: 123454, date: 'September 01, 2017' }
+    ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AgGridModule } from 'ag-grid-angular';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgGridModule.withComponents([])
   ],
   providers: [],
   bootstrap: [AppComponent]
